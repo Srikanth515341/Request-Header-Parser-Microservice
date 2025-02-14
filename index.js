7f384961fbdf739370fdc6c77491ba5dc912e8e7
Srikanth Nelluri <nellurisrikanthchowdary007@gmail.com>
2025-02-14 21:27:28 +0100

    Initial commit - Request Header Parser Microservice

--- index.js
+++ index.js
@@ -1,37 +1,38 @@
 // index.js
 // where your node app starts
 
-// init project
+// Load environment variables
 require('dotenv').config();
 var express = require('express');
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
+// Enable CORS for freeCodeCamp testing
 var cors = require('cors');
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 })); // Some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
+// Serve static files
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
+// Serve the homepage
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
+// Test API endpoint
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.get('/api/whoami',(req,res) =>{
-  let yourIP = req.ip;
+// Your main API endpoint for freeCodeCamp
+app.get('/api/whoami', (req, res) => {
+  let yourIP = req.headers["x-forwarded-for"]?.split(',')[0] || req.socket.remoteAddress || req.connection.remoteAddress || req.ip;
   let yourLanguage = req.header("accept-language");
   let yourSoftware = req.header("user-agent");
-  res.json({ipaddress : yourIP, language : yourLanguage, software : yourSoftware});
+
+  res.json({ ipaddress: yourIP, language: yourLanguage, software: yourSoftware });
 });
 
-// listen for requests :)
+// Listen for requests
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
